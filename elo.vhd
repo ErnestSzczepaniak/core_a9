b3fd4ea0e505cf708f22a8c526bab3fbec9f7310
en2 <ernest.szczepaniak.en2@gmail.com>
2020-03-13 11:09:06 +0100

    usuniecie elo

---
+++ elo.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity elo is
+  port (
+  
+  );
+end entity elo;
+
+
+architecture rtl of elo is
+  
+begin
+  
+  main_proc: process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = rst_val then
+        
+      else
+        
+      end if;
+    end if;
+  end process main_proc;
+  
+end architecture rtl;
